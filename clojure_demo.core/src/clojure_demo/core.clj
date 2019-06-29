1b3ef1ee6bef2379980c82ca8be425a499d8f54f
PaulGureghian <paulgureghian@att.net>
2019-06-29 15:53:28 -0700

    First commit

---
+++ clojure_demo.core/src/clojure_demo/core.clj
@@ -0,0 +1,13 @@
+;; Created by Paul A. Gureghian in June 2019. ;
+
+(ns clojure-demo.core
+  (:gen-class))
+
+(defn -main
+  "I am getting better in Clojure."
+  [& args]
+  (println "I am a Clojurian"))
+
+(defn hello-clojurians []
+    (println "Hello Clojurians"))
+(hello-clojurians)
---
+++ clojure_demo.core/src/clojure_demo/main.clj
@@ -0,0 +1,5 @@
+(ns clojure.examples.hello
+  (:gen-class))
+(defn hello-clojurians []
+    (println "Hello Clojurians"))
+(hello-clojurians)
